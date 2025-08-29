0d86e87a2da63fcc7de895796622a51c67c91e2c
hayubi <hayubi53@gmail.com>
2025-08-29 12:42:19 +0500

    Add files via upload

---
+++ Lab02/q2.cpp
@@ -0,0 +1,76 @@
+/*Task #2:
+Write a program that uses a dynamic 2D array to represent a seating chart in a classroom.
+=> Rows represent benches, columns represent seats on each bench.
+=> Initialize the array with 0 (empty).
+=> Let the user fill seats (1 for occupied).
+=> Print the final seating chart in matrix form.*/
+
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int **arr;
+    int rows, cols;
+    cout << "Enter rows: ";
+    cin >> rows;
+    cout << "Enter columns: ";
+    cin >> cols;
+
+    arr = new int*[rows];
+    for (int i=0; i<rows; i++)
+    {
+        arr[i] = new int[cols];
+    }
+
+    for (int i=0; i<rows; i++)
+    {
+        for (int j=0; j<cols; j++)
+        {
+            arr[i][j] = 0;
+        }
+    }
+
+    cout << "Do you want to fill seats? ";
+    char choice;
+    cin >> choice;
+    if (choice == 'y') 
+    {
+        cout << "How many seats do you want to fill? ";
+        int size;
+        cin >> size;
+        cout << "Enter the positions: " << endl;
+        for (int i=0; i<size; i++)
+        {
+            cout << i+1 << ". ";
+            int pos, r, c;
+            cin >> pos;
+            if (pos >= (rows*cols))
+            {
+                cout << "Boundary error";
+                return;
+            }
+            else
+            {
+                r = pos/cols;
+                c = (pos%cols)-1;
+                arr[r][c] = 1;
+            }
+        }
+    }
+
+    for (int i=0; i<rows; i++)
+    {
+        for (int j=0; j<cols; j++)
+        {
+            cout << arr[i][j] << " ";
+        }
+        cout << endl;
+    }
+
+    for (int i=rows-1; i>0; i--)
+    {
+        delete[] arr[i];
+    }
+    delete[] arr;
+}
