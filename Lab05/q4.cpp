f209f717dbf24b1cd1c6c2b6742ae9c59acf2785
hayubi <hayubi53@gmail.com>
2025-09-28 20:50:30 +0500

    Create q4.cpp

---
+++ Lab05/q4.cpp
@@ -0,0 +1,32 @@
+/*Task 4:
+Write a C++ program that implements the Ackermann function, which is a classic example of
+nested recursion. The function ackermann(int m, int n) is defined as:
+● If m=0, return n+1.
+● If m>0 and n=0, return ackermann(m-1, 1).
+● If m>0 and n>0, return ackermann(m-1, ackermann(m, n-1)).
+This task requires you to trace the execution and understand how one recursive call is a
+parameter to another.*/
+
+#include <iostream>
+using namespace std;
+
+int ackermann(int m, int n)
+{
+    if (m==0)
+        return n+1;
+    if (m>0 && n==0)
+        return ackermann(m-1, 1);
+    if (m>0 && n>0) 
+        return ackermann(m-1, ackermann(m, n-1));
+}
+
+int main()
+{
+    int m, n;
+    cout << "Enter m: ";
+    cin >> m;
+    cout << "Enter n: ";
+    cin >> n;
+    int res = ackermann(m,n);
+    cout << res << endl;
+}
